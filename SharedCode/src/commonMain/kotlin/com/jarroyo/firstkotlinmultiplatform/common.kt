fb083f97e9357f005509352a1e42ca8926f83162
Javier Arroyo <javier.arroyo@microhealth.com>
2019-05-29 16:26:07 +0200

    Removed unused code

--- SharedCode/src/commonMain/kotlin/com/jarroyo/firstkotlinmultiplatform/common.kt
+++ SharedCode/src/commonMain/kotlin/com/jarroyo/firstkotlinmultiplatform/common.kt
@@ -1,20 +1,7 @@
 package com.jarroyo.kotlinmultiplatform
 
-import com.jarroyo.kotlinmultiplatform.domain.model.CurrentWeather
-import com.jarroyo.kotlinmultiplatform.source.network.WeatherApi
-
 
 expect fun platformName(): String
-
-fun requestData(success: (CurrentWeather) -> Unit, failure: (Throwable?) -> Unit) {
-
-  WeatherApi().getCurrentWeather(success = {
-    success(it)
-
-  }, failure = {
-    failure(it)
-  })
-}
 
 
 
