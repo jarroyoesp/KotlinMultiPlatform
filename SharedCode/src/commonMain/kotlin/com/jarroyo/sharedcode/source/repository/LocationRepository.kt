ddd2e2bb87d90a5629ac1eb01e5dd1141629f2c2
Javier Arroyo <javier.arroyo@microhealth.com>
2019-08-09 09:51:40 +0200

    [LOCATION REP] Refactor database creation

--- SharedCode/src/commonMain/kotlin/com/jarroyo/sharedcode/source/repository/LocationRepository.kt
+++ SharedCode/src/commonMain/kotlin/com/jarroyo/sharedcode/source/repository/LocationRepository.kt
@@ -1,51 +1,49 @@
 package com.jarroyo.sharedcode.repository
 
 
+import com.jarroyo.kotlinmultiplatform.source.disk.dao.LocationDao
 import com.jarroyo.sharedcode.ApplicationDispatcher
+import com.jarroyo.sharedcode.Database
 import com.jarroyo.sharedcode.data.LocationModel
 import com.jarroyo.sharedcode.domain.Response
 import com.jarroyo.sharedcode.domain.model.Location
 import com.jarroyo.sharedcode.source.disk.DatabaseCreator
 import com.jarroyo.sharedcode.source.disk.DbArgs
-import com.jarroyo.kotlinmultiplatform.source.disk.dao.LocationDao
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class LocationRepository(
-    private val dbArgs: DbArgs
+    dbArgs: DbArgs
 ) {
 
+    private var mDatabase: Database = DatabaseCreator.getDataBase(dbArgs)
+
     suspend fun insertLocation(location: Location): Response<List<LocationModel>> {
-        val database = DatabaseCreator.getDataBase(dbArgs)
-        val locationDao = LocationDao(database)
+        val locationDao = LocationDao(mDatabase)
         locationDao.insert(location)
         return Response.Success(locationDao.select())
     }
 
     suspend fun getLocationList(): Response<List<LocationModel>> {
-        val database = DatabaseCreator.getDataBase(dbArgs)
-        val locationDao = LocationDao(database)
+        val locationDao = LocationDao(mDatabase)
         return Response.Success(locationDao.select())
     }
 
     suspend fun deleteLocation(location: Location): Response<List<LocationModel>> {
-        val database = DatabaseCreator.getDataBase(dbArgs)
-        val locationDao = LocationDao(database)
+        val locationDao = LocationDao(mDatabase)
         locationDao.delete(location)
         return Response.Success(locationDao.select())
     }
 
     fun selectFromDb() {
-        val database = DatabaseCreator.getDataBase(dbArgs)
-        val locationDao = LocationDao(database)
+        val locationDao = LocationDao(mDatabase)
         locationDao.select()
     }
 
     fun saveAsync(location: Location, success: (List<LocationModel>) -> Unit, failure: (Throwable?) -> Unit) {
         GlobalScope.launch(ApplicationDispatcher) {
             try {
-                val database = DatabaseCreator.getDataBase(dbArgs)
-                val locationDao = LocationDao(database)
+                val locationDao = LocationDao(mDatabase)
                 locationDao.insert(location)
 
                 val listLocation = getLocationList() as Response.Success
@@ -58,8 +56,7 @@ class LocationRepository(
 
     fun getLocationListAsync(success: (List<LocationModel>) -> Unit, failure: (Throwable?) -> Unit) {
         GlobalScope.launch(ApplicationDispatcher) {
-            val database = DatabaseCreator.getDataBase(dbArgs)
-            val locationDao = LocationDao(database)
+            val locationDao = LocationDao(mDatabase)
             val locationList = locationDao.select()
             success(locationList)
         }
